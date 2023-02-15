a6acaf6ae2cfa9887feac2d06337174c97a9fbcc
Calin Neamtu <nilac8991@gmail.com>
2023-02-15 12:45:18 +0100

    Implement input logic & finish overall development
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/nilac/mx/staticipaddressutility/EMDKUtility.kt
+++ app/src/main/java/com/zebra/nilac/mx/staticipaddressutility/EMDKUtility.kt
@@ -28,7 +28,7 @@ object EMDKUtility {
         EMDKLoader.getInstance().initEMDKManager(context, emdkManagerInitCallBack)
     }
 
-    private fun processProfile(
+    fun processProfile(
         ssid: String,
         wpaMode: Int,
         wifiPassword: String,
@@ -39,40 +39,42 @@ object EMDKUtility {
         processProfileResult: ProcessProfileResult
     ) {
         val profile = """
-            <characteristic type="ProfileInfo">
-                <parm name="created_wizard_version" value="11.0.1" />
-            </characteristic>
-            <characteristic type="Profile">
-                <parm name="ProfileName" value="IPStaticAddress" />
-                <parm name="ModifiedDate" value="2023-02-14 16:29:46" />
-                <parm name="TargetSystemVersion" value="11.1" />
-            
-                <characteristic type="Wi-Fi" version="11.1">
-                    <parm name="UseRegulatory" value="0" />
-                    <parm name="UseDiagnosticOptions" value="0" />
-                    <parm name="UseAdvancedOptions" value="0" />
-                    <parm name="NetworkAction" value="Add" />
-                    <characteristic type="network-profile">
-                        <parm name="SSID" value="$ssid" />
-                        <parm name="SecurityMode" value="1" />
-                        <parm name="WPAModePersonal" value="$wpaMode" />
-                        <characteristic type="key-details">
-                            <parm name="KeyType" value="Passphrase" />
-                            <parm name="ProtectKey" value="0" />
-                            <parm name="PassphraseWPAClear" value="$wifiPassword" />
-                        </characteristic>
-                        <parm name="UseDHCP" value="0" />
-                        <characteristic type="ip-details">
-                            <parm name="IpAddress" value="$ipAddress" />
-                            <parm name="IpGateway1" value="$gateway" />
-                            <parm name="IpMask" value="$subnetMask" />
-                            <parm name="IpDns1" value="$primaryDns" />
-                        </characteristic>
-                        <parm name="UseProxy" value="0" />
-                    </characteristic>
-                    <parm name="UseHotspotOptions" value="0" />
+            <wap-provisioningdoc>
+                <characteristic type="ProfileInfo">
+                    <parm name="created_wizard_version" value="11.0.1" />
                 </characteristic>
-            </characteristic>
+                <characteristic type="Profile">
+                    <parm name="ProfileName" value="IPStaticAddress" />
+                    <parm name="ModifiedDate" value="2023-02-14 16:29:46" />
+                    <parm name="TargetSystemVersion" value="11.1" />
+    
+                    <characteristic type="Wi-Fi" version="11.1">
+                        <parm name="UseRegulatory" value="0" />
+                        <parm name="UseDiagnosticOptions" value="0" />
+                        <parm name="UseAdvancedOptions" value="0" />
+                        <parm name="NetworkAction" value="Add" />
+                        <characteristic type="network-profile">
+                            <parm name="SSID" value="$ssid" />
+                            <parm name="SecurityMode" value="1" />
+                            <parm name="WPAModePersonal" value="$wpaMode" />
+                            <characteristic type="key-details">
+                                <parm name="KeyType" value="Passphrase" />
+                                <parm name="ProtectKey" value="0" />
+                                <parm name="PassphraseWPAClear" value="$wifiPassword" />
+                            </characteristic>
+                            <parm name="UseDHCP" value="0" />
+                            <characteristic type="ip-details">
+                                <parm name="IpAddress" value="$ipAddress" />
+                                <parm name="IpGateway1" value="$gateway" />
+                                <parm name="IpMask" value="$subnetMask" />
+                                <parm name="IpDns1" value="$primaryDns" />
+                            </characteristic>
+                            <parm name="UseProxy" value="0" />
+                        </characteristic>
+                        <parm name="UseHotspotOptions" value="0" />
+                    </characteristic>
+                </characteristic>
+            </wap-provisioningdoc>
         """
 
         ProfileLoader().processProfile(
@@ -94,7 +96,51 @@ object EMDKUtility {
             })
     }
 
-    private interface ProcessProfileResult {
+    fun processProfile(ssid: String, processProfileResult: ProcessProfileResult) {
+        val profile = """
+            <wap-provisioningdoc>
+                <characteristic type="ProfileInfo">
+                    <parm name="created_wizard_version" value="11.0.1" />
+                </characteristic>
+                <characteristic type="Profile">
+                    <parm name="ProfileName" value="RemoveSSID" />
+                    <parm name="ModifiedDate" value="2023-02-15 09:38:17" />
+                    <parm name="TargetSystemVersion" value="11.1" />
+    
+                    <characteristic type="Wi-Fi" version="11.1">
+                        <parm name="UseRegulatory" value="0" />
+                        <parm name="UseDiagnosticOptions" value="0" />
+                        <parm name="UseAdvancedOptions" value="0" />
+                        <parm name="NetworkAction" value="Remove" />
+                        <characteristic type="network-profile">
+                            <parm name="SSID" value="$ssid" />
+                        </characteristic>
+                        <parm name="UseHotspotOptions" value="0" />
+                    </characteristic>
+                </characteristic>
+            </wap-provisioningdoc>
+        """
+
+        ProfileLoader().processProfile(
+            "RemoveSSID",
+            profile,
+            object : ProfileLoaderResultCallback {
+                override fun onProfileLoadFailed(errorObject: EMDKResults) {
+                    //Nothing to see here..
+                }
+
+                override fun onProfileLoadFailed(message: String) {
+                    Log.e(TAG, "Failed to process profile!\n$message")
+                    processProfileResult.onProcessed(false)
+                }
+
+                override fun onProfileLoaded() {
+                    processProfileResult.onProcessed(true)
+                }
+            })
+    }
+
+    interface ProcessProfileResult {
         fun onProcessed(isProcessed: Boolean)
     }
 }
--- app/src/main/java/com/zebra/nilac/mx/staticipaddressutility/MainActivity.kt
+++ app/src/main/java/com/zebra/nilac/mx/staticipaddressutility/MainActivity.kt
@@ -2,21 +2,36 @@ package com.zebra.nilac.mx.staticipaddressutility
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.symbol.emdk.EMDKResults
 import com.zebra.nilac.emdkloader.interfaces.EMDKManagerInitCallBack
 import com.zebra.nilac.emdkloader.interfaces.ProfileLoaderResultCallback
+import com.zebra.nilac.mx.staticipaddressutility.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinder: ActivityMainBinding
+    private lateinit var wpaModesAdapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinder.root)
 
-        processProfile()
+        wpaModesAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.WPAModes)
+        )
+
+        mBinder.wpaModeInput.setAdapter(wpaModesAdapter)
+        mBinder.processButton.setOnClickListener {
+            processWifiRemovalProfile(mBinder.ssidInput.text.toString())
+        }
     }
 
-    private fun processProfile() {
+    private fun processWifiRemovalProfile(ssid: String) {
         EMDKUtility.initEMDKManager(DefaultApplication.getInstance(),
             object : EMDKManagerInitCallBack {
                 override fun onFailed(message: String) {
@@ -28,7 +43,69 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onSuccess() {
-                    //TODO
+                    EMDKUtility.processProfile(
+                        ssid,
+                        object : EMDKUtility.ProcessProfileResult {
+                            override fun onProcessed(isProcessed: Boolean) {
+                                processWifiProfile(
+                                    mBinder.ssidInput.text.toString(),
+                                    wpaModesAdapter.getPosition(mBinder.wpaModeInput.text.toString()) + 1,
+                                    mBinder.wifiPasswordInput.text.toString(),
+                                    mBinder.ipAddressInput.text.toString(),
+                                    mBinder.gatewayInput.text.toString(),
+                                    mBinder.subnetMaskInput.text.toString(),
+                                    mBinder.primaryDnsInput.text.toString()
+                                )
+                            }
+                        }
+                    )
+                }
+            })
+    }
+
+    private fun processWifiProfile(
+        ssid: String,
+        wpaMode: Int,
+        wifiPassword: String,
+        ipAddress: String,
+        gateway: String,
+        subnetMask: String,
+        primaryDns: String
+    ) {
+        EMDKUtility.initEMDKManager(DefaultApplication.getInstance(),
+            object : EMDKManagerInitCallBack {
+                override fun onFailed(message: String) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Failed to init EMDK Manager!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onSuccess() {
+                    EMDKUtility.processProfile(
+                        ssid,
+                        wpaMode,
+                        wifiPassword,
+                        ipAddress,
+                        gateway,
+                        subnetMask,
+                        primaryDns,
+                        object : EMDKUtility.ProcessProfileResult {
+                            override fun onProcessed(isProcessed: Boolean) {
+                                runOnUiThread {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        if (isProcessed) {
+                                            "Profile has been successfully processed!"
+                                        } else {
+                                            "Unable to process the profile!"
+                                        }, Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
+                        }
+                    )
                 }
             })
     }
